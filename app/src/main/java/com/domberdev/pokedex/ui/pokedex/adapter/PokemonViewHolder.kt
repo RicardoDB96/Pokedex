7915575e0bd4e5afbe8b3de16a16eeef752051a1
Ricardo DB <ricardo.domber96@gmail.com>
2023-12-13 23:08:50 -0600

    Improving PokedexViewHolder with binding and adding image load

--- app/src/main/java/com/domberdev/pokedex/ui/pokedex/adapter/PokemonViewHolder.kt
+++ app/src/main/java/com/domberdev/pokedex/ui/pokedex/adapter/PokemonViewHolder.kt
@@ -1,20 +1,19 @@
 package com.domberdev.pokedex.ui.pokedex.adapter
 
 import android.view.View
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.domberdev.pokedex.R
+import com.domberdev.pokedex.databinding.ItemPokemonBinding
 import com.domberdev.pokedex.domain.model.Pokemon
+import com.squareup.picasso.Picasso
 
 class PokemonViewHolder(view: View) : ViewHolder(view) {
 
-    val pokemonImage = view.findViewById<ImageView>(R.id.ivPokemon)
-    val pokemonName = view.findViewById<TextView>(R.id.tvPokemonName)
-    val pokemonNumber = view.findViewById<TextView>(R.id.tvPokemonNumber)
+    private val binding = ItemPokemonBinding.bind(view)
 
     fun render(pokemonModel: Pokemon) {
-        pokemonName.text = pokemonModel.name
-        pokemonNumber.text = itemView.context.getString(R.string.pokemon_number, pokemonModel.id)
+        binding.tvPokemonName.text = pokemonModel.name
+        binding.tvPokemonNumber.text = itemView.context.getString(R.string.pokemon_number, pokemonModel.id)
+        Picasso.get().load(pokemonModel.imageUrl).into(binding.ivPokemon)
     }
 }
