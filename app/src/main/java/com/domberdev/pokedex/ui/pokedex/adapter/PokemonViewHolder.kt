1674b9ba9a522c90eabfd83ac419ef26217ede5f
Ricardo DB <ricardo.domber96@gmail.com>
2023-12-14 19:29:17 -0600

    Small changes to item_pokemon and PokemonViewHolder, to improve the Pokedex RecyclerView

--- app/src/main/java/com/domberdev/pokedex/ui/pokedex/adapter/PokemonViewHolder.kt
+++ app/src/main/java/com/domberdev/pokedex/ui/pokedex/adapter/PokemonViewHolder.kt
@@ -12,7 +12,7 @@ class PokemonViewHolder(view: View) : ViewHolder(view) {
     private val binding = ItemPokemonBinding.bind(view)
 
     fun render(pokemonModel: Pokemon) {
-        binding.tvPokemonName.text = pokemonModel.name
+        binding.tvPokemonName.text = pokemonModel.name.replaceFirstChar { it.uppercase() }
         binding.tvPokemonNumber.text = itemView.context.getString(R.string.pokemon_number, pokemonModel.id)
         Picasso.get().load(pokemonModel.imageUrl).into(binding.ivPokemon)
     }
